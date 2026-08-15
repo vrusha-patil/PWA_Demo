0034776834df6660f1798bfed869763e5db92498
vrusha-patil <pvrushali9067@gmail.com>
2026-08-15 23:32:56 +0530

    for render deployment changes for mysql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,22 +1,30 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
-    port : process.env.DB_PORT
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
 
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
-    if(err)
-    {
-        console.error("Database Connection failed....",err.message);
-        return
+    if (err) {
+        console.error(
+            "Database Connection failed....",
+            err.message
+        );
+        return;
     }
-        console.log("Database Connection Successfull...");
+
+    console.log(
+        "Database Connection Successful..."
+    );
 });
 
-
-module.exports =db;
+module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,19 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173"
+            // Add your Render frontend URL here after deployment
+        ],
+        credentials: true
+    })
+);
 
 app.use(express.json());
 
 const db = require("./config/database");
-
 
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
@@ -21,15 +26,13 @@ const authorizeRoles = require("./middleware/roleMiddleware");
 
 const storeRoutes = require("./routes/storeRoutes");
 const ratingRoutes = require("./routes/ratingRoutes");
-
 const ownerRoutes = require("./routes/ownerRoutes");
 
-app.get("/",(req,res)=>
-{
+app.get("/", (req, res) => {
     res.send("Our Store Rating application API is running....");
 });
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/api/protected", authenticateToken, (req, res) => {
     res.json({
@@ -37,7 +40,6 @@ app.get("/api/protected", authenticateToken, (req, res) => {
         user: req.user
     });
 });
-
 
 app.use("/api/admin", adminRoutes);
 
@@ -59,7 +61,6 @@ app.use("/api/ratings", ratingRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server Running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
