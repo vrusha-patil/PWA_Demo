bb6cae3dcd7c98851c0c5551b1bf013e521ab6e4
vrusha-patil <pvrushali9067@gmail.com>
2026-09-16 13:45:20 +0530

    for PWA

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.use(cors({
     origin: [
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://pwa-demo-backend.onrender.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ const isLocalhost =
 const api = axios.create({
     baseURL: isLocalhost
         ? "http://localhost:5000/api"
-        : "https://demo-render-mysql-hosting-backend.onrender.com/api",
+        : "",
         
 
     headers: {
